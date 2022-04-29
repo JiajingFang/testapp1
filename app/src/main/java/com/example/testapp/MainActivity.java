dc7c4329ea7f6874f4190ea347399f359ff3960b
JiajingFang <krisssfong@gmail.com>
2022-04-29 16:52:58 +0200

    v1

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.testapp;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
+    private Button sendNotice;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendNotice=(Button)findViewById(R.id.button2);
+        sendNotice.setOnClickListener(this);
+    }
+    public void onClick(View v)
+    {
+        Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT).show();
+        return;
+    }
+
+
+}
